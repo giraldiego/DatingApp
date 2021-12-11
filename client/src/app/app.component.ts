f0dcc9ab59060393601fd44966aece52b7a929bd
giraldiego <giraldiego@gmail.com>
2021-12-11 15:08:13 -0500

    Add basic list display for users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'The Dating App';
+  users: any;
+  columns: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users')
+      .subscribe({
+        next: data => {
+          this.users = data;
+          this.columns = Object.keys(data[0]);
+        },
+        error: err => {
+          console.log(err.error);
+        }
+      })
+  }
 }
