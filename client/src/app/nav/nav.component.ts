e69f42b363975ff8508e1ec244917f125c85d4b9
giraldiego <giraldiego@gmail.com>
2021-12-15 06:14:16 -0500

    Add persistent storage

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,6 +13,7 @@ export class NavComponent implements OnInit {
   constructor(private accountService:AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
@@ -26,6 +27,18 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe({
+      next: user => {
+        this.loggedIn = !!user;
+      },
+      error: err => {
+        console.log(err.error.message);
+      }
+    })
   }
 }
