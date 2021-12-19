9562bc7e020497622df58d5db3d2fa7e8aa45ad4
giraldiego <giraldiego@gmail.com>
2021-12-18 21:32:42 -0500

    Map photoUrls to Members

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -12,7 +12,10 @@ namespace API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(
+                    src => src.Photos.FirstOrDefault(p => p.IsMain).Url
+                    ));
             CreateMap<Photo, PhotoDto>();
         }
     }
